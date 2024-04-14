97e88cb9cc2d607b1db3ecc3096f19f31a1e36ce
Łukasz <lukaszkogut1996@gmail.com>
2024-04-14 20:28:52 +0200

    shot-cart

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// import './style.css'
+document.addEventListener('DOMContentLoaded', function () {
+	const itemsList = document.getElementById('items-list')
+	const totalAmount = document.getElementById('total-amount')
+	let total = 0
 
-// const menuIcon = document.querySelector('.menu-icon')
-// const mainItem = document.querySelector('.main-item')
-// const menuContainer = document.querySelector('.menu-container')
-// const menuItems = document.querySelectorAll('.menu-item')
+	function addToCart(itemName, price) {
+		const item = document.createElement('div')
+		item.classList.add('item')
+		item.innerHTML = `<span>${itemName}</span> - <span>${price} zł</span>`
+		itemsList.appendChild(item)
+		total += price
+		totalAmount.textContent = total.toFixed(2)
+	}
 
-
-// function menuToggle() {
-//     menuContainer.classList.toggle('active')
-//     menuIcon.classList.toggle('active')
-// }
-
-// mainItem.addEventListener('click', menuToggle)
-
-// menuItems.forEach(item => {
-//     item.addEventListener('click', menuToggle)
-// })
+})
